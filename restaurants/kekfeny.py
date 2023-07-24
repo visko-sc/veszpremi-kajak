39d95365a2c14e33bc88cdff796593ee287ea966
Peter Viskovics <peter.viskovics@strivacity.com>
2023-07-24 19:31:26 +0200

    új étterem: kékfény + apró javítások

--- restaurants/kekfeny.py
+++ restaurants/kekfeny.py
@@ -11,20 +11,18 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36
              ' Chrome/114.0.0.0 Safari/537.36'  # else we get 403 :(
 
 
-class Panorama:
+class Kekfeny:
     def __init__(self):
         today = datetime.today().strftime('%Y-%m-%d')
         self.download_dir = f'{DOWNLOAD_DIR}/{today}'
-        self.file_path = f'{self.download_dir}/panorama.jpg'
+        self.file_path = f'{self.download_dir}/kekfeny.png'
         Path(self.download_dir).mkdir(parents=True, exist_ok=True)
         if not os.path.exists(self.file_path):
             self._download_menu_from_website()
 
     def _download_menu_from_website(self):
-        week_of_the_year = datetime.today().isocalendar()[1]
         with open(self.file_path, 'wb') as image_file:
-            # be aware that pdf exists (instead of ocr?)
-            image_file.write(requests.get(f'http://www.panoramasnack.hu/images/menu{week_of_the_year}.jpg',
+            image_file.write(requests.get('https://www.etel-hazhozszallitas.hu/images/etlap/etlap.png',
                                           headers={'User-Agent': USER_AGENT}).content)
 
     def lines(self):
@@ -34,4 +32,4 @@ class Panorama:
 
 
 if __name__ == '__main__':
-    print(Panorama().lines())
+    print(Kekfeny().file_path)
